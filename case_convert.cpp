2bb53bacf9afcedbdbeb1ea742c6cef9253b5d32
akashbhatkar <akashbhatkar11@gmail.com>
2022-09-28 16:47:45 +0530

    Converting cases of string

---
+++ case_convert.cpp
@@ -0,0 +1,28 @@
+//WAP for converting the case from the string 
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Enter the string "<<endl;
+    getline(cin,str);
+
+    string::iterator it1;
+    for (it1=str.begin();it1!=str.end();it1++)
+    {
+    if((*it1>=65)&&(*it1<=90))
+    {
+        *it1=*it1+32;
+
+    }
+    else if((*it1>=97)&&(*it1<=122))
+    {
+        *it1=*it1-32;
+
+    }
+    }
+    cout<<str<<endl;
+}
